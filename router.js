b13bd2ec50a606af3481277a9dc4135c502f261e
Jack Heriz-Smith <jackherizsmith@gmail.com>
2020-04-22 17:04:09 +0100

    add router file
    Relates #1
    
    Co-authored-by: lizzy-j <lizzyhj@hotmail.com>

---
+++ router.js
@@ -0,0 +1,52 @@
+function router() {
+  function clickHandler(event) {
+    if (
+      event.target.tagName === "A" &&
+      event.button === 0 &&
+      event.target.altKey === false &&
+      event.target.shiftKey === false &&
+      event.target.ctrlKey === false &&
+      event.target.metaKey === false
+    ) {
+      event.preventDefault();
+      window.history.pushState(null, null, href);
+      navigate(event.target.href);
+    }
+  }
+
+  const routes = {};
+
+  function get(path, callback) {
+    routes[path] = callback;
+  }
+
+  function navigate(url) {
+    const parsedUrl = new URL(url);
+    const callback = routes[parsedUrl.pathname];
+    callback({ url: parsedUrl, redirect: redirect });
+  }
+
+  function redirect(path) {
+    const url = window.location.origin + path;
+    window.history.pushState(null, null, url);
+    navigate(url);
+  }
+
+  function popHandler() {
+    navigate(window.location);
+  }
+
+  function listen() {
+    window.addEventListener("click", clickHandler);
+    window.addEventListener("popstate", popHandler);
+  }
+
+  function close() {
+    window.removeEventListener("click", clickHandler);
+    window.removeEventListener("popstate", popHandler);
+  }
+
+  return { get, listen, close };
+}
+
+export default router;
