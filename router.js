35cd3b15dcf71a9940a1c88e899cc15d05d41a75
Lizzy-j <lizzyhj@hotmail.com>
2020-04-23 15:16:45 +0100

    refactor login/signup routes to fix bug Relates #5
    Co-authored-by: tacotoemeck <wagner.tomek@gmail.com>

--- router.js
+++ router.js
@@ -8,17 +8,14 @@ function router() {
   function navigate(url) {
     const parsedUrl = new URL(url);
     const callback = routes[parsedUrl.pathname] || routes.default;
-
     callback({ url: parsedUrl, redirect });
   }
 
   function redirect(path) {
     const url = window.location.origin + path;
     window.history.pushState(null, null, url);
-    console.log(url);
+
     navigate(url);
-    // HTMLFormControlsCollection.lo
-    console.log(url);
   }
 
   function clickHandler(event) {
