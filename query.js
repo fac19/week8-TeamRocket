b216654e695a6a9c0e14927b6c07fd8e81418f85
Jack Heriz-Smith <jackherizsmith@gmail.com>
2020-04-23 10:55:25 +0100

    add query handler for fetch across project
    Relates #2
    
    Co-authored-by: tacotoemeck <wagner.tomek@gmail.com>

---
+++ query.js
@@ -0,0 +1,18 @@
+function query(url, options) {
+  fetch(url, options)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Server error");
+      }
+      const contentType = response.headers.get("content-type");
+      if (contentType && contentType.includes("json")) {
+        return response.json();
+      } else {
+        return response.text();
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+}
+export default query;
