d0c7aeaf197a9fc52f6708f6e8076a7c089ad282
Jack Heriz-Smith <jackherizsmith@gmail.com>
2020-04-24 08:41:39 +0100

    changes

--- routes/home.js
+++ routes/home.js
@@ -1,30 +1,71 @@
 import query from "../query.js";
 
+function dogToggle() {
+  const dogs = document.querySelectorAll(".dog");
+  dogs.forEach((dog) => {
+    dog.classList.toggle("dog--hidden");
+  });
+}
+
 function createDogElement(dogArr) {
   const app = document.querySelector("#app");
+  const pageHeading = document.createElement("h2");
+  pageHeading.textContent = "Bark up the right tree today 🦴";
+  pageHeading.classList.add("see_dogs__heading");
+  const toggle = document.createElement("button");
+  toggle.textContent = "See my doggo";
+  toggle.classList.add("see-dogs__toggle");
+  toggle.addEventListener("click", dogToggle);
+
+  toggle.addEventListener("click", () => {
+    if (toggle.textContent === "See my doggo") {
+      toggle.textContent = "See other doggos";
+    } else {
+      toggle.textContent = "See my doggo";
+    }
+  });
+
+  app.appendChild(pageHeading);
+  app.append(toggle);
   dogArr.map((dog) => {
     const dogCard = document.createElement("article");
-    const name = document.createElement("h3");
+    const name = document.createElement("h2");
     const breed = document.createElement("p");
     const photo = document.createElement("img");
-    const owner = document.createElement("p");
+    const owner = document.createElement("a");
 
     dogCard.classList.add("dog");
+    if (dog.owner == localStorage.getItem("id")) {
+      dogCard.classList.add("dog--hidden");
+    }
+
+    dogCard.classList.add("dog__card");
     name.classList.add("dog__name");
     breed.classList.add("dog__breed");
     photo.classList.add("dog__photo");
     owner.classList.add("dog__owner");
+    owner.href = "";
+    owner.id = dog.owner;
 
     name.innerText = dog.name;
     breed.innerText = dog.breed;
     photo.src = dog.image;
     photo.alt = "photo of " + dog.name;
-    owner.innerText = dog.owner;
+    owner.innerText = "Contact owner";
+    owner.addEventListener("click", (event) => {
+      event.preventDefault;
+      fetch("https://dogs-rest.herokuapp.com/v1/users/" + event.target.id, {})
+        .then((response) => response.json())
+        .then((data) => {
+          owner.innerText = data.email;
+        })
+        .catch(console.error);
+    });
 
+    dogCard.appendChild(owner);
+    dogCard.appendChild(photo);
     dogCard.appendChild(name);
     dogCard.appendChild(breed);
-    dogCard.appendChild(photo);
-    dogCard.appendChild(owner);
 
     app.appendChild(dogCard);
   });
@@ -34,7 +75,6 @@ function home() {
   fetch("https://dogs-rest.herokuapp.com/v1/dogs/", {})
     .then((response) => response.json())
     .then((dogs) => {
-      console.log(dogs);
       createDogElement(dogs);
     })
     .catch(console.error);
