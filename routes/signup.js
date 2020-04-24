1de56e7c761e973a12608e94cffce306a2ae62d3
Ina Yoon <itsina96@gmail.com>
2020-04-24 01:45:02 +0100

    god knows how many changes I made in css

--- routes/signup.js
+++ routes/signup.js
@@ -2,13 +2,13 @@ const app = document.querySelector("#app");
 
 const html = /*html*/ `<form class="form">
 
-<label for="name" class="form__label">Username</label>
+<label for="name" class="form__label">Username:</label>
 <input type="text" name="name" id="name" class="form__input" required>
 
-<label for="email" class="form__label">Email</label>
+<label for="email" class="form__label">Email:</label>
 <input type="email" name="email" id="email" class="form__input" required>
 
-<label for="password" class="form__label" >Password</label>
+<label for="password" class="form__label" >Password:</label>
 <input type="password" name="password" id="password" class="form__input" required>
 
 <button type="submit" class="submit-btn">Submit</button>
@@ -24,7 +24,6 @@ function signUp({ redirect }) {
     const formData = new FormData(e.target);
     const formObject = Object.fromEntries(formData);
     fetch("https://dogs-rest.herokuapp.com/v1/users/", {
-
       method: "POST",
       body: JSON.stringify(formObject),
 
